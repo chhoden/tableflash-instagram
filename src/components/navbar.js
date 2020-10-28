1f842750647acea10ea32eab48fc4a725db4f52b
chhoden <chhoden.grg@hotmail.com>
2020-10-27 23:04:39 -0700

    initial commit

---
+++ src/components/navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './navbar.css';
+const NavBar = () => {
+    return (
+        <p>
+            This is a navbar
+        </p>
+    );
+}
+
+export { NavBar };
---
+++ src/containers/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+
+    return (
+        <p>
+            This is the home page
+        </p>
+    );
+
+}
+
+export { Home };
